175ef1b1b68545144f0eb99cb4c3d34e79050a44
Math Debauve <mathdebauve@gmail.com>
2022-02-14 11:45:32 +0100

    Ajout du mode 1 fonctionnel ! (+ compteur)

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 ##Intro
+from random import randint
+
+
 print( "Bienvenue au juste nombre !")
 print( "----- MENU -----")
 print( "[1] Jouer")
 print( "[2] Scores")
 print( "[3] Quitter")
 
+cpt = 0
 
 ## Menu
 c_menu = int(input("Choisissez une option :"))
@@ -16,23 +20,38 @@ if c_menu == 1 :
   print("Bienvenue", prenom, "!") 
   print("Je m'appelle MathMw et je suis le maître du jeu du 'juste nombre'.\nLe but est de trouver le nombre que je vais générer aléatoirement.\nje te propose 3 modes de jeu :\n- Niveau 1 : De 1 à 10.\n- Niveau 2 : De 1 à 100.\n- Niveau 3 : De 1 à 1000.")
   print("Le but est de trouver le nombre que je vais générer aléatoirement.")
-  c_mode = int(input("Quel mode veux-tu choisir ?"))
+  c_level = int(input("Quel niveau veux-tu choisir ?"))
 
-  if c_mode == 1 :
-      print("mode 1 !")
+  if c_level == 1 :
+      print("D'accord, allons-y !")
+      rdm_nbre = randint(1,10)
+      print(rdm_nbre)
 
-if c_mode == 2 :
+      
+      while True:
+         nbre = int(input("Quel est ton nombre ?"))
+         cpt = cpt + 1
+         if nbre == rdm_nbre :
+            print("Bravo ! Tu as trouvé en ",cpt," coups !")
+            break
+         if cpt == 5 :
+            print("Dommage, tu as utilisé tes 5 coups, tu as perdu !")
+
+
+
+
+if c_level == 2 :
       print("mode 2 !")
 
 
-if c_mode == 3 :
+if c_level == 3 :
       print("mode 3 !")      
 
 
 ## Scores
 
 if c_menu == 2 :
-    fichier = open("data.txt", "r")
+    fichier = open("scores.txt", "r")
     print(fichier.read())
     fichier.close()
 
